e05c237154cff4c43ed7398cf98d55db2f693648
Sandro Kavlashvili <kavlashvilisandro1@gmail.com>
2025-09-26 15:59:26 +0400

    Add extension method for rabbit adding

--- DotNetCore.SharpStreamer.RabbitMq/SharpStreamerRabbitMqExtensions.cs
+++ DotNetCore.SharpStreamer.RabbitMq/SharpStreamerRabbitMqExtensions.cs
@@ -1,6 +1,12 @@
+using Microsoft.Extensions.DependencyInjection;
+
 namespace DotNetCore.SharpStreamer.RabbitMq;
 
 public static class SharpStreamerRabbitMqExtensions
 {
-    
+    public static IServiceCollection AddSharpStreamerRabbitMq(this IServiceCollection services)
+    {
+        
+        return services;
+    }
 }
