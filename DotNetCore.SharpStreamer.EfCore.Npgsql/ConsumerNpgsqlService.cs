b3c32e17540fe64636a09c968d4ea14aa3318e55
unknown <kavlashvilisandro1@gmail.com>
2025-09-28 21:22:17 +0400

    Modify received events saver and add ConsumerService ind ependency

--- DotNetCore.SharpStreamer.EfCore.Npgsql/ConsumerNpgsqlService.cs
+++ DotNetCore.SharpStreamer.EfCore.Npgsql/ConsumerNpgsqlService.cs
@@ -1,6 +1,9 @@
-﻿using DotNetCore.SharpStreamer.Entities;
+﻿using System.Data;
+using Dapper;
+using DotNetCore.SharpStreamer.Entities;
 using DotNetCore.SharpStreamer.Services.Abstractions;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
 
 namespace DotNetCore.SharpStreamer.EfCore.Npgsql;
 
@@ -9,10 +12,33 @@ public class ConsumerNpgsqlService<TDbContext>(TDbContext dbContext) : IConsumer
 {
     public async Task SaveConsumedEvent(ReceivedEvent receivedEvent)
     {
-        dbContext.Set<ReceivedEvent>().Add(receivedEvent);
-        await dbContext.SaveChangesAsync();
-
-        // detach the entity so it's no longer tracked
-        dbContext.Entry(receivedEvent).State = EntityState.Detached;
+        const string insertQuery = $@"
+                            INSERT INTO sharp_streamer.received_events
+                            (
+                                ""Id"",
+                                ""Topic"",
+                                ""Content"",
+                                ""RetryCount"",
+                                ""SentAt"",
+                                ""Timestamp"",
+                                ""ExpiresAt"",
+                                ""Status"",
+                                ""Topic""
+                            )
+                            VALUES
+                            (
+                                @{nameof(ReceivedEvent.Id)},
+                                @{nameof(ReceivedEvent.Topic)},
+                                @{nameof(ReceivedEvent.Content)}::json,
+                                @{nameof(ReceivedEvent.RetryCount)},
+                                @{nameof(ReceivedEvent.SentAt)},
+                                @{nameof(ReceivedEvent.Timestamp)},
+                                @{nameof(ReceivedEvent.ExpiresAt)},
+                                @{nameof(ReceivedEvent.Status)},
+                                @{nameof(ReceivedEvent.Topic)}
+                            );";
+        IDbConnection dbConnection = dbContext.Database.GetDbConnection();
+        IDbTransaction? dbTransaction = dbContext.Database.CurrentTransaction?.GetDbTransaction();
+        await dbConnection.ExecuteAsync(sql: insertQuery, param: receivedEvent, transaction: dbTransaction);
     }
 }
--- DotNetCore.SharpStreamer.RabbitMq/Jobs/EventsProcessor.cs
+++ DotNetCore.SharpStreamer.RabbitMq/Jobs/EventsProcessor.cs
@@ -1,8 +1,9 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using DotNetCore.SharpStreamer.Services.Abstractions;
+using Microsoft.Extensions.Hosting;
 
 namespace DotNetCore.SharpStreamer.RabbitMq.Jobs;
 
-public class EventsProcessor : BackgroundService
+public class EventsProcessor(IConsumerService consumerService) : BackgroundService
 {
     protected override Task ExecuteAsync(CancellationToken stoppingToken)
     {
