d673f3ea1b7576c3ad2e6f3916f40087dbf8094c
unknown <kavlashvilisandro1@gmail.com>
2025-09-28 20:39:56 +0400

    Added dependensies

---
+++ DotNetCore.SharpStreamer.RabbitMq/Jobs/EventsProcessor.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace DotNetCore.SharpStreamer.RabbitMq.Jobs;
+
+public class EventsProcessor : BackgroundService
+{
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        throw new NotImplementedException();
+    }
+}
