40e57d63dbde38e9c7cc092b8853b072ca864efa
SandrikaMgeli <kavlashvilisandro1@gmail.com>
2025-10-16 23:24:52 +0400

    Add kafka init projects

---
+++ DotNetCore.SharpStreamer.Kafka.Npgsql/KafkaNpgDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using DotNetCore.SharpStreamer.Storage.Npgsql;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetCore.SharpStreamer.Kafka.Npgsql;
+
+public class KafkaNpgDbContext(DbContextOptions<KafkaNpgDbContext> options) : DbContext(options)
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        modelBuilder.ConfigureSharpStreamerNpgsql();
+        base.OnModelCreating(modelBuilder);
+    }
+}
