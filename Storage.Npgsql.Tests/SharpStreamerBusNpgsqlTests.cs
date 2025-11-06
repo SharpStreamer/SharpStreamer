6ed4cf300033757d75ebbe741aff248475c31373
SandrikaMgeli <kavlashvilisandro1@gmail.com>
2025-11-06 19:11:29 +0400

    remove dummy tests for sharp streamer bus

--- Storage.Npgsql.Tests/SharpStreamerBusNpgsqlTests.cs
+++ Storage.Npgsql.Tests/SharpStreamerBusNpgsqlTests.cs
@@ -5,15 +5,5 @@ namespace Storage.Npgsql.Tests;
 [Collection(nameof(GlobalCollection))]
 public class SharpStreamerBusNpgsqlTests(PostgresDbFixture postgresDbFixture) : IClassFixture<PostgresDbFixture>
 {
-    [Fact]
-    public void Test1()
-    {
-        Assert.True(true);
-    }
-
-    [Fact]
-    public void Test2()
-    {
-        Assert.True(true);
-    }
+    
 }
